4c2a0382ae37d3a36591913205e687e88275273b
vanchay01 <chaydouble0@gmail.com>
2026-08-30 15:58:50 +0700

    haha

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Work extends Model
 {
     //
+    protected $fillable = [
+        'name', 
+        'postion',
+        'github',
+        'demo', 
+        'framework',
+        'description'
+    ];
+
+    function features() : HasMany {
+        return $this->hasMany(
+            feature
+        )
+    };
 }
